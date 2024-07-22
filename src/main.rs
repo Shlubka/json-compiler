0b17ebb429a4c883d328092baaa82c14036a8df8
Shlubka <kirillkostin2020@gmial.com>
2024-07-23 00:13:25 +0300

    сделал работу с ключами

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![warn(clippy::all, clippy::pedantic)]
 use inquire::{Select, Text};
+use std::env;
 //use rust_fuzzy_search::{fuzzy_compire};
 use std::io::BufRead;
 use std::{
@@ -40,33 +41,53 @@ impl Language for Rust {
         };
         let reader = BufReader::new(file);
 
-        let mut mystack: Vec<char> = Vec::new();
-        println!("stach {}", mystack.len());
+        /*let keyword: Vec<String> = vec![
+            String::from("return"),
+            String::from("fn main"),
+            String::from("fn"),
+            String::from("if"),
+            String::from("else"),
+            String::from("let"),
+            String::from("for"),
+            String::from("loot"),
+            String::from("while"),
+            String::from("match"),
+            String::from("print"),
+            String::from("}"),
+            String::from(")"),
+            String::from("]"),
+            //String::from(""),
+        ];*/
+
+        //println!("stach {}", mystack.len());
         let mut i = 1;
+        let mut mystack: Vec<char> = Vec::new();
         let mut external_func: Vec<String> = Vec::new();
         for line in reader.lines() {
-            let line = line?;     //переаисать эту хуйню через match
-            if line.contains("}") { //and mystack.top() == '{'
+            let line = line?; //переаисать эту хуйню через match
+            if line.contains("}") {
+                //and mystack.top() == '{'
                 mystack.pop();
             }
-            if line.contains("fn main") { // создать 2 вектора, и рогнать их либо чеоез for либо
-                                          // через iter
+            if line.contains("fn main") {
+                // создать 2 вектора, и рогнать их либо чеоез for либо
+                // через iter
                 print!("Line {i:>3} have enter point      ");
                 mystack.push('{')
             } else if line.contains("fn") {
                 print!("Line {i:>3} have 'fn'             ");
                 mystack.push('{');
                 let func_name = line
-                .split_whitespace()
-                .nth(1)
-                .unwrap()
-                .split('(')
-                .next()
-                .unwrap();
-            external_func.push(func_name.to_string());
+                    .split_whitespace()
+                    .nth(1)
+                    .unwrap()
+                    .split('(')
+                    .next()
+                    .unwrap();
+                external_func.push(func_name.to_string());
             } else if line.contains("return") {
                 print!("Line {i:>3} have exit from fn     ");
-                mystack.push('{');
+                //mystack.push('{');
             } else if let Some(external_func) = external_func.iter().find(|&kw| line.contains(kw)) {
                 print!("Line {i:>3} have call of {:<7}  ", external_func)
             } else if line.contains("let") {
@@ -74,24 +95,22 @@ impl Language for Rust {
                 continue;
             } else if line.len() == 0 {
                 i += 1;
-                //continue;
+                continue;
             } else if line.contains("if") {
                 print!("Line {i:>3} have if               ");
                 mystack.push('{');
             } else if line.contains("else") {
                 print!("Line {i:>3} have else             ");
                 mystack.push('{');
-            }
-            else {
+            } else {
                 print!("Line {i:>3} have action           ")
             }
             println!("| {}      {line}", mystack.len());
             i += 1;
         }
-    if mystack.len() > 0 {
-        println!("stack more than 0")
-    }
-
+        if mystack.len() > 0 {
+            println!("stack more than 0")
+        }
         Ok(())
     }
 }
@@ -127,6 +146,15 @@ impl Language for Java {
 }
 
 fn main() -> Result<(), std::io::Error> {
+    let args: Vec<String> = env::args().collect();
+    let lang = match args.len() > 1 {
+        true => args[1].clone(),
+        false => "default_lang".to_string(), // или любое другое значение по умолчанию
+    };
+    let file_path = match args.len() > 2 {
+        true => args[2].clone(),
+        false => "default_file_path".to_string(), // или любое другое значение по умолчанию
+    };
     let support_language: Vec<Box<dyn Language>> = vec![
         Box::new(Rust),
         Box::new(C),
@@ -134,13 +162,26 @@ fn main() -> Result<(), std::io::Error> {
         Box::new(Java),
     ];
 
-    let selected_language: &str = Select::new(
-        "Language?",
-        support_language.iter().map(|x| x.get_name()).collect(),
-    )
-    .prompt()
-    .unwrap();
-    let path = PathBuf::from(Text::new("Path").prompt().unwrap());
+    let selected_language: &str;
+    match lang.as_str() {
+        "rust" => selected_language = "Rust",
+        "java" => selected_language = "Rust",
+        "cpp" => selected_language = "CPlusPlus",
+        "c" => selected_language = "C",
+        _ => {
+            selected_language = Select::new(
+                "Language?",
+                support_language.iter().map(|x| x.get_name()).collect(),
+            )
+            .prompt()
+            .unwrap();
+        }
+    }
+
+    let path = match file_path.as_str() {
+        "default_file_path" => PathBuf::from(Text::new("Path").prompt().unwrap()),
+        _ => PathBuf::from(file_path.clone()),
+    };
 
     let selected_language = support_language
         .iter()
--- src/test.rs
+++ src/test.rs
@@ -1,12 +1,11 @@
-    fn extun() -> i32 {
+  fn extun() -> i32 {
     let a:i32 = 12;
     let b:i32 = 21;
     if a > b {
         a - b
     }
     else {
-        return b - a
-    }
+        return b - a}
 }
 
 fn main () {
