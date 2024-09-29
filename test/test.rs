9f630e8eb877836a479dbec4d8430fe51a479eaa
Shlubka <kirillkostin2020@gmial.com>
2024-09-29 12:41:32 +0300

    в процессе переезда на tree-sitter

--- test/test.rs
+++ test/test.rs
@@ -1,19 +1,11 @@
-fn goyda() {
-    if a > b {
-        println!("a > b");
-        println!("a > b");
-        println!("a > b");
-        println!("a > b");
-        println!("a > b");
-    }
-    else {
-        println!("a < b");
-    }
-}
-
 fn main() -> i32 {
-    goyda(3, 5);
+    let gg = 0;
 
+    if true {
+        println!("true")
+    } else {
+        println!("false")
+    }
     for i in 1..10 {
         println!("all ok");
     }
