afcd4dbb15e4de3818cc9890598ae2c3b2e89865
Shlubka <kirillkostin2020@gmial.com>
2024-10-05 10:39:36 +0300

    починил проверку на скобки

--- test/test.rs
+++ test/test.rs
@@ -1,11 +1,6 @@
 fn main() -> i32 {
     let gg = 0;
 
-    if true {
-        println!("true")
-    } else {
-        println!("false")
-    }
     for i in 1..10 {
         println!("all ok");
     }
