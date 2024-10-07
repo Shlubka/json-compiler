d915badd52e2cc008321929636714bfd1379454d
Shlubka <kirillkostin2020@gmial.com>
2024-10-07 12:49:50 +0300

    вместо флага циклов теперь счетчик, начал делать стрекли относительно вложенности циклов (потом прикручу еще и if/else); научился обрабатывать if,loop, while; пора садиться за if/else с неизвестной вложенностью

--- test/test.rs
+++ test/test.rs
@@ -1,8 +1,60 @@
 fn main() -> i32 {
     let gg = 0;
-
     for i in 1..10 {
         println!("all ok");
+
+        for i in 1..10 {
+            println!("all ok");
+        }
+
+        loop {
+            for i in 1..10 {
+                println!("all ok");
+
+                for i in 1..10 {
+                    println!("all ok");
+                }
+
+                loop {
+                    println!("loop");
+                }
+
+                while i < 10 {
+                    println!("{i}");
+                    i += 1;
+                }
+            }
+        }
+
+        while i < 10 {
+            println!("{i}");
+            i += 1;
+        }
+    }
+
+    loop {
+        println!("loop");
+    }
+
+    while i < 10 {
+        println!("{i}");
+        i += 1;
+    }
+    match gg {
+        1 => println!("1"),
+        2 => println!("2"),
+        3 => println!("3"),
+        4 => println!("4"),
+        5 => println!("5"),
+        5 => println!("5"),
+        5 => println!("5"),
+        5 => println!("5"),
+        5 => println!("5"),
+        5 => println!("5"),
+        5 => println!("5"),
+        5 => println!("5"),
+        5 => println!("5"),
+        5 => println!("5"),
     }
     return 1;
 }
