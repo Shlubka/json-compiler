25f43cd3844de2ad2492034741261dfa5db03a08
Shlubka <kirillkostin2020@gmial.com>
2024-10-14 00:01:27 +0300

    на новом тестовом коде все поехало к хуям блять. надо порешать

--- test/test.rs
+++ test/test.rs
@@ -1,82 +1,61 @@
+fn goyda() -> bool {
+    let condition1 = true;
+    let condition2 = true;
+    let condition3 = true;
+    let condition4 = false;
+    let condition6 = false;
+    let condition8 = false;
+
+    if condition1 {
+        println!("statement1");
+        if condition2 {
+            println!("statement2");
+            if condition3 {
+                println!("statement3");
+            } else if condition4 {
+                println!("statement4");
+            } else {
+                println!("statement5");
+            }
+        } else if condition6 {
+            println!("statement6");
+        } else {
+            println!("statement7");
+        }
+    } else if condition8 {
+        println!("statement8");
+    } else {
+        println!("statement9");
+    }
+
+    true
+}
+
 fn main() -> i32 {
     let gg = 0;
 
-
     for i in 1..10 {
         println!("all ok");
-
-        for i in 1..10 {
-            println!("all ok");
-        }
-
-        loop {
-            for i in 1..10 {
-                println!("all ok");
-
-                for i in 1..10 {
-                    println!("all ok");
-                }
-
-                loop {
-                    println!("loop");
-                }
-
-                while i < 10 {
-                    println!("{i}");
-                    i += 1;
-                }
-            }
-        }
-
-        while i < 10 {
-            println!("{i}");
-            i += 1;
-        }
     }
 
     loop {
-        if (condition1) {
-            statement1;
-            if (condition2) {
-                statement2;
-                if (condition3) {
-                    statement3;
-                } else if (condition4) {
-                    statement4;
-                } else {
-                    statement5;
-                }
-            } else if (condition6) {
-                statement6;
-            } else {
-                statement7;
-            }
-        } else if (condition8) {
-            statement8;
-        } else {
-            statement9;
-        }
+        goyda();
+        break; // Добавлено для предотвращения бесконечного цикла
     }
 
-    while i < 10 {
-        println!("{i}");
-        i += 1;
+    while gg < 10 {
+        println!("{}", gg);
+        break; // Добавлено для предотвращения бесконечного цикла
     }
+
     match gg {
         1 => println!("1"),
         2 => println!("2"),
         3 => println!("3"),
         4 => println!("4"),
         5 => println!("5"),
-        5 => println!("5"),
-        5 => println!("5"),
-        5 => println!("5"),
-        5 => println!("5"),
-        5 => println!("5"),
-        5 => println!("5"),
-        5 => println!("5"),
-        5 => println!("5"),
-        5 => println!("5"),
+        _ => println!("other"),
     }
+
     return 1;
 }
